a9cff5cd461b504ab59ec3c48e031bc02252ce5c
prima3b <prima3b@2c92d0ab-4e4e-9033-e898-2ff0bac84692>
2014-03-25 11:23:58 +0000

    - added check internet connection

--- trunk/Car Service/CarService/CarService/build/source/r/debug/pl/rzeszow/wsiz/carservice/R.java
+++ trunk/Car Service/CarService/CarService/build/source/r/debug/pl/rzeszow/wsiz/carservice/R.java
@@ -867,7 +867,6 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f070015;
         public static final int action_menu_presenter=0x7f070016;
         public static final int action_mode_close_button=0x7f070024;
-        public static final int action_settings=0x7f070047;
         public static final int activity_chooser_view_content=0x7f070025;
         public static final int always=0x7f07000f;
         public static final int beginning=0x7f07000a;
@@ -958,16 +957,11 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int activity_main=0x7f030017;
         public static final int fragment_main=0x7f030018;
-        public static final int login=0x7f030019;
-        public static final int register=0x7f03001a;
-        public static final int splash=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
-    }
-    public static final class menu {
-        public static final int login=0x7f0c0000;
-        public static final int main=0x7f0c0001;
-        public static final int register=0x7f0c0002;
-        public static final int splash=0x7f0c0003;
+        public static final int guest=0x7f030019;
+        public static final int login=0x7f03001a;
+        public static final int register=0x7f03001b;
+        public static final int splash=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class string {
         public static final int abc_action_bar_home_description=0x7f0a0000;
@@ -985,15 +979,15 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000c;
         public static final int action_settings=0x7f0a000d;
         public static final int app_name=0x7f0a000e;
-        public static final int guest=0x7f0a000f;
-        public static final int hello_world=0x7f0a0010;
-        public static final int hint_pass=0x7f0a0011;
-        public static final int hint_usr_name=0x7f0a0012;
-        public static final int login=0x7f0a0013;
-        public static final int name=0x7f0a0014;
-        public static final int password=0x7f0a0015;
-        public static final int register=0x7f0a0016;
-        public static final int remember_pass=0x7f0a0017;
+        public static final int check=0x7f0a000f;
+        public static final int guest=0x7f0a0010;
+        public static final int hello_world=0x7f0a0011;
+        public static final int login=0x7f0a0012;
+        public static final int name=0x7f0a0013;
+        public static final int password=0x7f0a0014;
+        public static final int register=0x7f0a0015;
+        public static final int remember_pass=0x7f0a0016;
+        public static final int title_activity_guest=0x7f0a0017;
         public static final int title_activity_login=0x7f0a0018;
         public static final int title_activity_register=0x7f0a0019;
         public static final int title_activity_splash=0x7f0a001a;
--- trunk/Car Service/CarService/CarService/src/main/java/pl/rzeszow/wsiz/carservice/JSONParser.java
+++ trunk/Car Service/CarService/CarService/src/main/java/pl/rzeszow/wsiz/carservice/JSONParser.java
@@ -1,14 +1,6 @@
 package pl.rzeszow.wsiz.carservice;
 
-/**
- * Created by prima on 19.03.14.
- */
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.List;
+import android.util.Log;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -22,7 +14,12 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.util.Log;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 public class JSONParser {
 
@@ -30,11 +27,9 @@ public class JSONParser {
     static JSONObject jObj = null;
     static String json = "";
 
-    // constructor
     public JSONParser() {
 
     }
-
 
     public JSONObject getJSONFromUrl(final String url) {
 
@@ -92,7 +87,6 @@ public class JSONParser {
         return jObj;
 
     }
-
 
     // function get json from url
     // by making HTTP POST or GET mehtod
